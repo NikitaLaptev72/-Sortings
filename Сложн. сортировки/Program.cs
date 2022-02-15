055eeb20a3d12706f3f0c64a95fe3ffeb4b0ee80
NikitaLaptev72 <68819814+NikitaLaptev72@users.noreply.github.com>
2022-02-15 08:09:49 +0500

    Add files via upload

---
+++ Сложн. сортировки/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сложн.сортировки
+{
+    class Program
+    {
+        //Сортировка слиянием - требуется большая память
+        public static void Merger(int[] mass, int k, int q)
+        {
+            // k – нижняя граница упорядоченного фрагмента
+            // q – верхняя граница фрагмента
+            int i, j, t, mid;
+            int[] d = new int[20];
+            i = k;
+            mid = k + (q - k) / 2;
+            j = mid + 1;
+            t = 1;
+            while (i <= mid && j <= q)
+            {
+                if (mass[i] <= mass[j])
+                {
+                    d[t] = mass[i]; i++;
+                }
+                else
+                {
+                    d[t] = mass[j]; j++;
+                }
+                t++;
+            }
+            // Один из фрагментов обработан полностью, осталось перенести в D остаток другого фрагмента
+            while (i <= mid)
+            {
+                d[t] = mass[i]; i++; t++;
+            }
+            while (j <= q)
+            {
+                d[t] = mass[j]; j++; t++;
+            }
+
+            for (i = 1; i <= t - 1; i++)
+            {
+                mass[k + i - 1] = d[i];
+            }
+        }
+
+        // Рекурсивная реализация сортировки слиянием 
+        public static int[] Sort_Merger(int[] mass, int i, int j)
+        {
+            int t;
+            if (i < j)
+            {
+                // Обрабатываемый фрагмент массива состоит более, чем из одного элемента
+                if (j - i == 1)
+                {
+                    if (mass[j] < mass[i])
+                    // Обрабатываемый фрагмент массива состоит из двух элементов*)
+                    {
+                        t = mass[i]; mass[i] = mass[j]; mass[j] = t;
+                    };
+                }
+                else
+                {
+                    // Разбиваем заданный фрагмент на два
+                    Sort_Merger(mass, i, i + (j - i) / 2); // рекурсивные вызовы процедуры Sort_Sl
+                    Sort_Merger(mass, i + (j - i) / 2 + 1, j);
+                    Merger(mass, i, j);
+                }
+            }
+            return mass;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********Сортировка слиянием*********");
+            Random rnd = new Random();
+            Console.WriteLine("Исходный массив:");
+            int[] a = new int[20];
+            int n = 19;
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(1, 150);
+                Console.Write(a[i] + " ");
+            }
+            Sort_Merger(a, 0, n - 1);
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив:"); ;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Сортировки/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сортировки
+{
+    class Program
+    {
+        static int[] CreationArray(int[] keys, int count)
+        {
+            Random rnd = new Random();
+            keys = new int[count];
+            for (int i = 0; i < keys.Count(); i++)
+            {
+                keys[i] = rnd.Next(1, 150);
+            }
+            return keys;
+        }
+
+        static int[] ShellSort(int[] mass)
+        {
+            int i, j, step;
+            int tmp;
+            for (step = mass.Count() / 2; step > 0; step /= 2)
+            {
+                for (i = step; i < mass.Count(); i++)
+                {
+                    tmp = mass[i];
+                    for (j = i; j >= step; j -= step)
+                    {
+                        if (tmp < mass[j - step])
+                        {
+                            mass[j] = mass[j - step];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    mass[j] = tmp;
+                }
+            }
+            return mass;
+        }
+
+        static void Main(string[] args)
+        {
+            int count = 20;
+            int[] mass = new int[count];
+
+            mass = CreationArray(mass, count);
+            Console.WriteLine("*********Сортировка Шелла*********");
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(mass[i] + " ");
+            }
+            Console.WriteLine();
+            mass = ShellSort(mass);
+            Console.WriteLine("Отсортированный массив:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(mass[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
